389b578f39eb284d7f13a7c5f0ced1baf6eb4b38
Patricia Soto <patricia.soto@routinguc.com>
2021-10-27 16:51:51 -0300

    add useForm hook & handle login conection with back

--- src/types/types.js
+++ src/types/types.js
@@ -8,4 +8,11 @@ export const types = {
   eventClearActiveEvent: "[event] Clear active event",
   eventUpdated: "[event] Event updated",
   eventDeleted: "[event] Event deleted",
+
+  authCheckingFinish: '[auth] Finish checking login state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start Register',
+  authStartStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout',
 };
